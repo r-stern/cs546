088060551a5a4df58bd16b3afe316184533a8640
rachel stern <40131531+r-stern@users.noreply.github.com>
2019-04-08 01:24:49 -0400

    Update index.js

--- lab06/routes/index.js
+++ lab06/routes/index.js
@@ -1,1 +1,15 @@
+const aboutData = require("./about");
+const storyData = require("./story");
+const educationData = require("./education");
 
+const constructorMethod = app => {
+	app.use("/about", aboutData);
+	app.use("/story", storyData);
+	app.use("/education", educationData);
+
+	app.use('*', (req,res) => {
+		res.status(404).json({error: "Not Found"})
+	});
+};
+
+module.exports = constructorMethod;
