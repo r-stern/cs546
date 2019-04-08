563a04777901f7a32bd65404959905758f827cba
rachel stern <40131531+r-stern@users.noreply.github.com>
2019-04-08 01:24:08 -0400

    Update about.js

--- lab06/routes/about.js
+++ lab06/routes/about.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const router = express.Router();
 
+
+router.get("/", (req,res) => {
+	try {
+		const aboutData = {
+			"name": "rachel stern",
+			"cwid": "10413057",
+			"biography": "hi i'm rachel stern. i'm a cybersecurity major at stevens institute of technology, and i'm interested in art, finance, and agile.\nin my free time, i listen to music and play the piano.",
+			"favoriteShows": ["friends", "the end of the f***ing world", "bojack horseman", "exo next door"],
+			"hobbies": ["video games", "piano", "sleeping"]
+		}
+		res.json(aboutData);
+	} catch (e) {
+		res.status(500).send()
+	}
+});
+
+module.exports = router;
