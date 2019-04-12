1a281ebc16bab9e31f2578184e8fb59f10f73ef4
rachel stern <40131531+r-stern@users.noreply.github.com>
2019-04-12 01:17:38 -0400

    Update story.js

--- lab06/routes/story.js
+++ lab06/routes/story.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const router = express.Router();
 
+router.get("/", (req,res) => {
+	try {
+		const storyData = {
+			"storyTitle": "spring forward",
+			"story": "It's 3am right now but it should be 2am.\nI lost an hour and I'm very mad about it. \nAlso, I don't know what else to say, but I really wish I had that hour back."
+		}
+		res.json(storyData);
+	} catch (e) {
+		res.status(500).send()
+	}
+});
+
+module.exports = router;
